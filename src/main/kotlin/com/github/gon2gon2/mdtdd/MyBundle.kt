37a3e1ad05cc2d2f77ebd8f944f7e7f091c2d579
GitHub Action <action@github.com>
2024-02-16 03:43:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/gon2gon2/mdtdd/MyBundle.kt
+++ src/main/kotlin/com/github/gon2gon2/mdtdd/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gon2gon2.mdtdd
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gon2gon2/mdtdd/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gon2gon2/mdtdd/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gon2gon2.mdtdd.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
