51660a4d23d7d156b71a24fa169ad621d80a7fb1
Sunggon Song <ark5354@gmail.com>
2024-03-10 22:46:54 +0900

    chore: description, 메타인포 수정 (#15)
    Co-authored-by: gon-song <gon.song@balancehero.com>

--- src/main/kotlin/com/github/gon2gon2/mdtdd/MdtddToolWindowFactory.kt
+++ src/main/kotlin/com/github/gon2gon2/mdtdd/MdtddToolWindowFactory.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
-class TimerToolWindowFactory : ToolWindowFactory {
+class MdtddToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val timerToolWindow = UnifiedToolWindow(project)
         val contentFactory = ContentFactory.getInstance()
