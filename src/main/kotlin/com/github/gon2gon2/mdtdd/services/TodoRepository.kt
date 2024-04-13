61160945cd35acdccdd61fb8fb0199de475591ff
gon2gon2 <ark5354@gmail.com>
2024-04-13 18:42:23 +0900

    delete unnecessary files,test

--- src/main/kotlin/com/github/gon2gon2/mdtdd/services/TodoRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.github.gon2gon2.mdtdd.services
-
-interface TodoRepository {
-    fun add(todo: String)
-    fun remove(todo: String)
-    fun remove(todoIndex: Int)
-    fun getAll(): List<String>
-}
